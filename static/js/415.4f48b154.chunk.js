5156e0cb6ad3f02852d70931028db76aeaf3117e
MagisterUnivers <36455862+MagisterUnivers@users.noreply.github.com>
2023-05-02 11:12:20 +0000

    Deploying to gh-pages from @ MagisterUnivers/goit-react-hw-05-movies@05b685deee1b6898e01b95d3fb6d87c6611a0722 🚀

---
+++ static/js/415.4f48b154.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:function(e,r,o){o.r(r);var t=o(184);r.default=function(){return(0,t.jsxs)("div",{children:[(0,t.jsx)("h1",{children:"Welcome to FairyMovie\xae"}),(0,t.jsx)("h2",{children:"On our site you can search for a movie."}),(0,t.jsx)("h2",{children:"You can read about movie, see whos got the role in it, and see reviews for it."})]})}}}]);
+//# sourceMappingURL=415.4f48b154.chunk.js.map
